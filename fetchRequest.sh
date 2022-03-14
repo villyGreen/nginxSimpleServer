f150607353df903fac7e93ab5744735c9ff67754
villyGreen <vat.13@list.ru>
2022-03-14 15:04:37 +0700

    fetchRequest.sh

---
+++ fetchRequest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+curl 'https://api.telegram.org/bot5132986297:AAHAdbzO_XbTHQwRDyZD9d5PdBQ5yirVnIc/sendMessage?chat_id=1392161321&text=Project: '$1'%0ADate: '$2'%0AStage: '$3'%0AStatus: '$4''
+curl 'https://api.telegram.org/bot5132986297:AAHAdbzO_XbTHQwRDyZD9d5PdBQ5yirVnIc/sendMessage?chat_id=1392161321&text=Project: '$1'%0ADate: '$2'%0AStage: '$3'%0AStatus: '$4''
